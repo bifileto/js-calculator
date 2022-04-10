0dee4a0ad432afcc2111ceb73172550c4398a020
Bianca <bifileto@gmail.com>
2022-04-10 01:35:32 -0300

    Iniciei a coletar os Ids dos botoes conforme eles sao clicados

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let buttons = document.querySelectorAll('button');
+let buttonId;
+
+function getId (buttons){
+  buttonId = buttons.id;
+  console.log(buttonId);
+}
+//
+
+/*  function selecionar (){
+  let a = document.getElementById();
+  let b = document.getElementById();
+}*/
+
+function soma (a,b){
+  return a + b;
+}
