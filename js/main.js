2cdcca4ea98fc358d9c1721b5dd8c5594998463a
Anderson Silva <anderson_ns5@hotmail.com>
2022-04-11 00:09:25 -0300

    Atualização do display principal e controller das operações aritméticas

--- js/main.js
+++ js/main.js
@@ -1,17 +1,47 @@
 let buttons = document.querySelectorAll('button');
 let buttonId;
+var calculator = document.querySelector(".calculator");
+var display = calculator.querySelector(".display");
+var displayOld = display.querySelector("#dold");
+var displayNew = display.querySelector("#dnew");
+var memoriaGeral;
 
 function getId (buttons){
   buttonId = buttons.id;
   console.log(buttonId);
-}
-//
 
+  //Controller das operações aritméticas
+  switch (buttonId)
+  {
+    case "igual":
+      console.log("vai passar para memoria e mostrar no newDisplay e oldDisplay");
+    break;
+    case "sub":
+      console.log("vai acrescentar - e mostrar no newDisplay");
+    break;
+    case "sum":
+      console.log("vai acrescentar + e mostrar no newDisplay");
+    break;
+    case "prod":
+      console.log("vai acrescentar * e mostrar no newDisplay");
+    break;
+    case "div":
+      console.log("vai acrescentar / e mostrar no newDisplay");
+    break;
+    case "del":
+      console.log("vai deletar 1 campo da memoria e atualizar no newDisplay");
+    break;
+    case "ac":
+      console.log("vai deletar a memoria e atualizar no newDisplay");
+    break;
+  }
+  displayNew.textContent += buttons.value;
+}
 /*  function selecionar (){
   let a = document.getElementById();
   let b = document.getElementById();
 }*/
 
-function soma (a,b){
-  return a + b;
+function soma (newNumber,memory){
+  return memory + newNumber;
 }
